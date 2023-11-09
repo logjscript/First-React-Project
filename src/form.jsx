bdc92884454994279c781f48ca23dbb743604436
Logan Dietel <logandietel1@gmail.com>
2023-11-08 20:42:49 -0800

    Bug fixes

--- src/form.jsx
+++ src/form.jsx
@@ -9,22 +9,22 @@ export function Form({ stateValues }) {
         'Company Name': 'Company Name',
         'Position in Organization': 'Position',
         'Responsibilities': 'Responsibilities',
-      };
+    };
     
-      const generateFieldContent = (fieldName) => (
-        stateValues[fieldName] || '_________'
-      );
+    const placeholderContent = (fieldName) => (
+    stateValues[fieldName] || '_________'
+    );
 
     return (
         <div className='form-text'>
         <h1>Form</h1>
         <p>
-            Hello, my name is <b>{generateFieldContent('Name')}</b>, 
-            my email address is <b>{generateFieldContent('Email')}</b>,
-            and my phone number is <b>{generateFieldContent('Phone Number')}</b>.
-            I studied <b>{generateFieldContent('Field of Study')}</b> at <b>{generateFieldContent('School Name')}</b> in <b>{generateFieldContent('Date Attended')}</b>.
-            I currently work at <b>{generateFieldContent('Company Name')}</b> as a <b>{generateFieldContent('Position in Organization')}</b>. 
-            I handle <b>{generateFieldContent('Responsibilities')}</b>.
+            Hello, my name is <b>{placeholderContent('Name')}</b>, 
+            my email address is <b>{placeholderContent('Email')}</b>,
+            and my phone number is <b>{placeholderContent('Phone Number')}</b>.
+            I studied <b>{placeholderContent('Field of Study')}</b> at <b>{placeholderContent('School Name')}</b> in <b>{placeholderContent('Date Attended')}</b>.
+            I currently work at <b>{placeholderContent('Company Name')}</b> as a <b>{placeholderContent('Position in Organization')}</b>. 
+            I handle <b>{placeholderContent('Responsibilities')}</b>.
         </p>
     </div>
     );
