4ad0afc47db970578632a52399e67109c44ee54c
Logan Dietel <logandietel1@gmail.com>
2023-11-08 19:42:38 -0800

    Create App as root  component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,52 @@
 import { useState } from 'react'
 import './App.css'
+import { Section } from './section.jsx';
+import { Form } from './form.jsx'
 
 export default function App() {
-  
+  const [inputValues, setInputValues] = useState({
+    'Name': '',
+    'Email': '',
+    'Phone Number': '',
+    'School Name': '',
+    'Field of Study': '',
+    'Date Attended': '',
+    'Company Name': '',
+    'Position in Organization': '',
+    'Responsibilities': '',
+  });
+
+    function handleInputChange(value, name) {
+        setInputValues({
+            ...inputValues, 
+            [name]: value,
+        });
+    }
+  return (
+    <>
+      <Section
+        name='General Information'
+        inputNames={['Name', 'Email', 'Phone Number']}
+        stateValues={inputValues}
+        handleInput={handleInputChange}
+        />
+      <Section
+        name='Education'
+        inputNames={['School Name', 'Field of Study', 'Date Attended']}
+        stateValues={inputValues}
+        handleInput={handleInputChange}
+        />
+      <Section
+        name='Experience'
+        inputNames={['Company Name', 'Position in Organization', 'Responsibilities']}
+        stateValues={inputValues}
+        handleInput={handleInputChange}
+        />
+      <Form 
+        stateValues={inputValues}
+      />
+    </>
+  )
 }
 
 
