e35f1390d64bf420b63c8687abec9247743b1ac5
Logan Dietel <logandietel1@gmail.com>
2023-11-08 19:44:37 -0800

    Move state from Section to App

--- src/section.jsx
+++ src/section.jsx
@@ -1,33 +1,25 @@
 import { useState } from 'react'
 import { Input } from './input.jsx'
 
-export function Section({ name, inputNames }) {
-    const [inputValues, setInputValues] = useState({});
-
-    function handleInputChange(value, name) {
-        setInputValues({
-            ...inputValues, 
-            [name]: value,
-        });
-    }
+export function Section({ name, inputNames, stateValues, handleInput }) {
 
     return (
         <div className='section'>
             <h1>{name}</h1>
             <Input 
             name={inputNames[0]}
-            value={inputValues[inputNames[0]]}
-            handleFunction={handleInputChange}
+            value={stateValues[inputNames[0]]}
+            handleFunction={handleInput}
             />
             <Input 
             name={inputNames[1]}
-            value={inputValues[inputNames[1]]}
-            handleFunction={handleInputChange}
+            value={stateValues[inputNames[1]]}
+            handleFunction={handleInput}
             />
             <Input 
             name={inputNames[2]}
-            value={inputValues[inputNames[2]]}
-            handleFunction={handleInputChange}
+            value={stateValues[inputNames[2]]}
+            handleFunction={handleInput}
             />
         </div>
     )
