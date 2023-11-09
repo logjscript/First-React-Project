82c8c7738e260b8e7fc3ce94f1d018ea07b64683
Logan Dietel <logandietel1@gmail.com>
2023-11-08 20:43:26 -0800

    Use mapping to create Inputs

--- src/section.jsx
+++ src/section.jsx
@@ -1,26 +1,22 @@
 import { useState } from 'react'
 import { Input } from './input.jsx'
 
-export function Section({ name, inputNames, stateValues, handleInput }) {
+function makeInputs(inputNames, stateValues, handleInput) {
+    return inputNames.map((inputName, index) => (
+      <Input
+        key={index}
+        name={inputName}
+        value={stateValues[inputName]}
+        handleFunction={handleInput}
+      />
+    ));
+  }
 
+export function Section({ name, inputNames, stateValues, handleInput }) {
     return (
         <div className='section'>
-            <h1>{name}</h1>
-            <Input 
-            name={inputNames[0]}
-            value={stateValues[inputNames[0]]}
-            handleFunction={handleInput}
-            />
-            <Input 
-            name={inputNames[1]}
-            value={stateValues[inputNames[1]]}
-            handleFunction={handleInput}
-            />
-            <Input 
-            name={inputNames[2]}
-            value={stateValues[inputNames[2]]}
-            handleFunction={handleInput}
-            />
+          <h1>{name}</h1>
+          {makeInputs(inputNames, stateValues, handleInput)}
         </div>
-    )
+      );
 }
